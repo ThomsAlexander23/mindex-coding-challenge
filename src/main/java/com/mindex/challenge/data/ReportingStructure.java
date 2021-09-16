519e402502e4f2fe061a3163cc263a883e2c384f
Alex <thoms.alexander23@gmail.com>
2021-09-16 14:14:52 -0400

    feat: constructors, getters, setters created for class ReportingStructure

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -2,4 +2,30 @@ package com.mindex.challenge.data;
 
 public class ReportingStructure {
 
+    private Employee employee;
+    private long numberOfReports;
+
+    public ReportingStructure(){}
+
+    public ReportingStructure(Employee employee, long numberOfReports){
+        this.employee = employee;
+        this.numberOfReports = numberOfReports;
+    }
+
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public long getNumberOfReports() {
+        return numberOfReports;
+    }
+
+    public void setNumberOfReports(long numberOfReports) {
+        this.numberOfReports = numberOfReports;
+    }
 }
