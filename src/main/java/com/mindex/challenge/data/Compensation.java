5e5704a19dbb3b26a396b1429b20a8597c6b0ae8
Alex <thoms.alexander23@gmail.com>
2021-09-16 22:47:20 -0400

    feat: Compensation data class filled in with contructor, getter, setter

--- src/main/java/com/mindex/challenge/data/Compensation.java
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -1,4 +1,42 @@
 package com.mindex.challenge.data;
 
+import java.util.Date;
+
 public class Compensation {
+
+    private Employee employee;
+    private Date effectiveDate;
+    private String salary;
+
+    public Compensation(){}
+
+    public Compensation(Employee employee, String salary, Date effectiveDate){
+        this.employee = employee;
+        this.salary = salary;
+        this.effectiveDate = new Date(effectiveDate.getTime());
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Date getEffectiveDate() {
+        return new Date(effectiveDate.getTime());
+    }
+
+    public void setEffectiveDate(Date effectiveDate) {
+        this.effectiveDate = new Date(effectiveDate.getTime());
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
 }
