be75e1470e1837b976d2047e1cbf9c5234769662
Alex <thoms.alexander23@gmail.com>
2021-09-17 12:35:54 -0400

    test: added test with successful completion for reports

--- src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
+import com.mindex.challenge.service.ReportingStructureService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +18,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,9 +26,14 @@ public class EmployeeServiceImplTest {
 
     private String employeeUrl;
     private String employeeIdUrl;
+    private String reportEmployeeId;
+    private String reportingStructureServiceUrl;
 
     @Autowired
     private EmployeeService employeeService;
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
 
     @LocalServerPort
     private int port;
@@ -38,6 +45,8 @@ public class EmployeeServiceImplTest {
     public void setup() {
         employeeUrl = "http://localhost:" + port + "/employee";
         employeeIdUrl = "http://localhost:" + port + "/employee/{id}";
+        reportingStructureServiceUrl = "http://localhost:" + port + "/employee/{id}/report";
+        reportEmployeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
     }
 
     @Test
@@ -47,6 +56,9 @@ public class EmployeeServiceImplTest {
         testEmployee.setLastName("Doe");
         testEmployee.setDepartment("Engineering");
         testEmployee.setPosition("Developer");
+
+
+
 
         // Create checks
         Employee createdEmployee = restTemplate.postForEntity(employeeUrl, testEmployee, Employee.class).getBody();
@@ -59,6 +71,17 @@ public class EmployeeServiceImplTest {
         Employee readEmployee = restTemplate.getForEntity(employeeIdUrl, Employee.class, createdEmployee.getEmployeeId()).getBody();
         assertEquals(createdEmployee.getEmployeeId(), readEmployee.getEmployeeId());
         assertEmployeeEquivalence(createdEmployee, readEmployee);
+
+        // Report Read Checks
+        ReportingStructure testReportWithReports = restTemplate.getForEntity(reportingStructureServiceUrl, ReportingStructure.class, reportEmployeeId).getBody();
+        ReportingStructure testReportWithNoReports = restTemplate.getForEntity(reportingStructureServiceUrl, ReportingStructure.class, createdEmployee.getEmployeeId()).getBody();
+
+        assertEquals(testReportWithReports.getNumberOfReports(), "2");
+        assertNotNull(testReportWithReports);
+        assertEquals(testReportWithNoReports.getNumberOfReports(), "0");
+        assertNotNull(testReportWithNoReports);
+        assertNotEquals(testReportWithReports, testReportWithNoReports);
+
 
 
         // Update checks
@@ -77,7 +100,8 @@ public class EmployeeServiceImplTest {
         assertEmployeeEquivalence(readEmployee, updatedEmployee);
     }
 
-    private static void assertEmployeeEquivalence(Employee expected, Employee actual) {
+
+    static void assertEmployeeEquivalence(Employee expected, Employee actual) {
         assertEquals(expected.getFirstName(), actual.getFirstName());
         assertEquals(expected.getLastName(), actual.getLastName());
         assertEquals(expected.getDepartment(), actual.getDepartment());
